52bbd499d229ab53fae2339ee8e815169941a16c
Magdiel <magmo922002@gmail.com>
2023-11-27 08:55:21 -0700

    Removed unused import

--- frontend/controlapp/src/app/students-subjects.service.ts
+++ frontend/controlapp/src/app/students-subjects.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Subject } from './subject';
 import { environment } from 'src/environments/environment';
 import { StudentsSubjects } from './students-subjects';
 
